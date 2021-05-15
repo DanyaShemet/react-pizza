54ba9ff247e55128cc72d81d3f5f751b6e1982a9
Daniil Shemet <shemetg@gmail.com>
2021-05-15 22:29:53 +0300

    half lessons 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import {Header} from "./components";
 import {Cart, Home} from "./pages";
 import {Route} from 'react-router-dom';
+import {useState, useEffect} from "react";
 
 
 function App() {
+
+    const [pizzas, setPizzas] = useState([], )
+
+    useEffect(() => {
+        fetch('http://localhost:3000/db.json').then(data => data.json()).then(json => {
+            setPizzas(json.pizzas)
+        })
+    }, [])
+
     return (
         <div className="wrapper">
             <Header />
             <div className="content">
-                <Route exact path='/' component={Home}/>
+                <Route exact path='/' render={() => <Home items={pizzas}/>}/>
                 <Route exact path='/cart' component={Cart}/>
             </div>
         </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {Header} from "./Header"
 export {Categories} from "./Categories"
 export {SortPopup} from "./SortPopup"
+export {PizzaItem} from "./PizzaItem"
